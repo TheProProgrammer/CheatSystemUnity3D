af9b5138f48a57f474be996dfbbc5c492a90ad6d
Abdullah Asim <45878555+TheProProgrammer@users.noreply.github.com>
2023-03-01 17:51:58 +0500

    Add files via upload

---
+++ CheatSys.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class CheatSys : MonoBehaviour
+{
+	string cheatBuffer;
+	char currentChar;
+	string[] cheats = {""};//Cheats Are Case Sensitive
+
+	void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			currentChar = Input.inputString.ToCharArray()[0];
+			cheatBuffer = cheatBuffer + currentChar;
+
+			for (int i = 0; i < cheats.Length ; i++)
+			{
+				if (cheatBuffer == cheats[i])
+				{
+                    print("A Cheat Was Registered");
+
+                    /*switch (cheatString)      Edit This Part According To Your Need
+                    {
+                        case "Cheat1":
+                            SomeFunction();
+                            break;
+            
+                        case "Cheat2":
+                            SomeFunction2();
+                            break;
+
+                                        case "Cheat2":
+                            SomeFunction2();
+                            break;
+                    }*/
+                }
+                else if (cheats [i].StartsWith (cheatBuffer))
+				{
+                    break;
+				}
+				else if (cheats[i].StartsWith(cheatBuffer) == false && i == cheats.Length-1)
+				{
+					cheatBuffer = "";
+				} 
+			}
+		}
+	}
+}
